0eb5e089ae2c0e8fc579983bb1e22a210a531806
Rafael <oliveirarafael22@outlook.com.br>
2025-04-24 04:04:54 -0300

    add router and cookies support with authentication initialization

--- src/main.js
+++ src/main.js
@@ -14,8 +14,23 @@ import App from './App.vue'
 // Composables
 import { createApp } from 'vue'
 
+// Cookies
+import Cookies from 'js-cookie';
+
+import { useAuthStore } from './stores/auth';
+// router
+import router from './router/index';
+
 const app = createApp(App)
 
 registerPlugins(app)
+
+app.use(router)
+
+app.use(Cookies)
+
+const authStore = useAuthStore()
+
+authStore.initializeAuth()
 
 app.mount('#app')
