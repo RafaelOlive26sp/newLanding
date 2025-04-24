c041bf344eb9f56b00aaa1a89f41c0c6a4f79dca
Rafael <oliveirarafael22@outlook.com.br>
2025-04-24 04:03:26 -0300

    add authentication service with login and logout functions

---
+++ src/services/auth.js
@@ -0,0 +1,16 @@
+import api from './api';
+
+
+export const login = async (credentials) => {
+
+
+  const response = await api.post('/api/login', credentials)
+  console.log('response da api login',response.data);
+
+  return response.data
+}
+
+export const logout = async () => {
+  const response = await api.post('/api/v1/logout')
+  return response.data
+}
