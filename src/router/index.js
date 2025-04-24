6e85ec0b6b52f2c25c5f9b69c5f5cd82f9f7c181
Rafael <oliveirarafael22@outlook.com.br>
2025-04-24 04:03:47 -0300

    refactor router configuration to remove placeholders and implement authentication checks

--- src/router/index.js
+++ src/router/index.js
@@ -9,37 +9,33 @@ import { createRouter, createWebHistory } from 'vue-router/auto'
 import { setupLayouts } from 'virtual:generated-layouts'
 import { routes as autoRoutes } from 'vue-router/auto-routes'
 import LandingPage from '../components/LandingPage.vue';
-import AboutPage from '../components/AboutPage.vue';
-
+import LoginView from '@/components/login/LoginView.vue';
+import LandingPageAluno from '@/components/Telaaluno/LandingPageAluno.vue';
+import { useAuthStore } from '../stores/auth.js';
 const manualRoutes = [
   { path: '/', component: LandingPage },
-  { path: '/sobre', component: AboutPage }, // Placeholder
-  { path: '/aulas', component: LandingPage }, // Placeholder
-  { path: '/login', component: LandingPage }, // Placeholder
+  { path: '/login', component: LoginView, meta: { guest: true } },
+  { path: '/home', component: LandingPageAluno, meta: { requiresAuth: true } },
 ];
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: setupLayouts([...autoRoutes, ...manualRoutes]),
 })
-//
-// // Workaround for https://github.com/vitejs/vite/issues/11804
-// router.onError((err, to) => {
-//   if (err?.message?.includes?.('Failed to fetch dynamically imported module')) {
-//     if (!localStorage.getItem('vuetify:dynamic-reload')) {
-//       console.log('Reloading page to fix dynamic import error')
-//       localStorage.setItem('vuetify:dynamic-reload', 'true')
-//       location.assign(to.fullPath)
-//     } else {
-//       console.error('Dynamic import error, reloading page did not fix it', err)
-//     }
-//   } else {
-//     console.error(err)
-//   }
-// })
+
 
 router.isReady().then(() => {
   localStorage.removeItem('vuetify:dynamic-reload')
+})
+
+// This function checks authentication before each route change
+router.beforeEach((to, from, next) => {
+  const authStore = useAuthStore()
+  if (to.meta.requiresAuth && !authStore.isAuthenticated){
+    next('/login')
+  }else{
+    next()
+  }
 })
 
 export default router
