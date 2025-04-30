96f2a923d0b7564724e6332f15548eea072d3236
Rafael <oliveirarafael22@outlook.com.br>
2025-04-30 04:14:17 -0300

    feat: add Snackbar component for displaying temporary messages

---
+++ src/components/snackBar/snackBarView.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="text-center ma-2">
+    <v-snackbar v-model="snackbar" timeout="7000">
+      {{ textContent }}
+      <template v-slot:actions>
+        <v-btn color="pink" variant="text" @click="snackbar = false">
+          Close
+        </v-btn>
+      </template>
+    </v-snackbar>
+  </div>
+
+</template>
+
+
+<script setup>
+  import { ref, watch } from 'vue'
+
+  const props = defineProps({
+    textContent:{
+      type: String,
+      default: 'Hello, I am a snackbar'
+    },
+    modelValue:{
+      type: Boolean,
+      required: true
+
+    }
+  })
+
+  defineEmits(["update:modelValue"]);
+  const snackbar = ref(props.modelValue)
+
+  watch(()=>props.modelValue,(newValue)=>{
+    snackbar.value = newValue
+  })
+  // const text = ref(`Hello, I'm a snackbar`)
+
+</script>
