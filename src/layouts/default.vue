8e8f30cc3420826443654de82b049edc2f7e1f6a
Rafael <oliveirarafael22@outlook.com.br>
2025-04-30 01:20:52 -0300

    feat: conditionally render AppFooter based on user authentication status

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -3,9 +3,18 @@
     <router-view />
   </v-main>
 
-  <AppFooter />
+  <AppFooter v-if="userLogado"/>
 </template>
 
 <script setup>
-  //
+  import { onMounted, computed } from 'vue';
+  import { useAuthStore } from '@/stores/auth';
+
+  const AuthStore = useAuthStore()
+  onMounted(() => {
+    userLogado.value
+  });
+  const userLogado = computed(() => {
+    return AuthStore.token !== null
+  });
 </script>
