366d14d3625fb5caa24b0df6c2eeb063402f9f96
Rafael Delgado <oliveirarafael22@outlook.com.br>
2025-05-02 17:25:15 -0300

    refactor: ajustado a intentaçã

--- src/stores/user.js
+++ src/stores/user.js
@@ -14,16 +14,12 @@ export const userUseStore = defineStore('user',{
     async completeProfileUser(response){
       try {
         this.completeProfile = response
-        console.log('response do completeProfile',this.completeProfile);
         if(response.status === 201){
           const userLocalStorage = JSON.parse(localStorage.getItem('user'));
-           userLocalStorage.CompleteStudentRecord = true
-
-           localStorage.setItem('user', JSON.stringify(userLocalStorage)) // atualiza os dados do localStorage
-            const useAuth = useAuthStore() // acessa o authStore
+          userLocalStorage.CompleteStudentRecord = true
+          localStorage.setItem('user', JSON.stringify(userLocalStorage)) // atualiza os dados do localStorage
+          const useAuth = useAuthStore() // acessa o authStore
           useAuth.user = userLocalStorage // quando acessa o authStore, vai ate a variavel user para atualizar conforme o localStore
-
-
         }
 
       } catch (error) {
