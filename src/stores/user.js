6abd2f9d33a140b8c9f7a08bd6909cb1404a9c13
Rafael <oliveirarafael22@outlook.com.br>
2025-05-01 21:11:08 -0300

    feat: enhance completeProfileUser action to update localStorage and authStore on successful profile completion

--- src/stores/user.js
+++ src/stores/user.js
@@ -1,11 +1,12 @@
 import { defineStore } from "pinia";
+import { useAuthStore } from '@/stores/auth';
 
 export const userUseStore = defineStore('user',{
-  state:()=>{
-    completeProfile:[]
-    responseCadastro:[]
+  state:()=>({
+    completeProfile:[],
+    responseCadastro:[],
 
-  },
+  }),
   getters:{
 
   },
@@ -13,6 +14,18 @@ export const userUseStore = defineStore('user',{
     async completeProfileUser(response){
       try {
         this.completeProfile = response
+        console.log('response do completeProfile',this.completeProfile);
+        if(response.status === 201){
+          const userLocalStorage = JSON.parse(localStorage.getItem('user'));
+           userLocalStorage.CompleteStudentRecord = true
+
+           localStorage.setItem('user', JSON.stringify(userLocalStorage)) // atualiza os dados do localStorage
+            const useAuth = useAuthStore() // acessa o authStore
+          useAuth.user = userLocalStorage // quando acessa o authStore, vai ate a variavel user para atualizar conforme o localStore
+
+
+        }
+
       } catch (error) {
         console.log(error);
 
