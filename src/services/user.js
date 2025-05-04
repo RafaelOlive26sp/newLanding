f9f8025ccdaee2cf2d0ea647f0f07aa6fcda0c08
Rafael <oliveirarafael22@outlook.com.br>
2025-05-03 21:21:45 -0300

    refactor: standardize code formatting and improve readability in user service

--- src/services/user.js
+++ src/services/user.js
@@ -1,20 +1,31 @@
-import api from './api'
-import Cookies from 'js-cookie';
-
+import api from "./api";
+import Cookies from "js-cookie";
 
 export const completeUser = async (data) => {
-  const getToken = Cookies.get('access_token');
-  const response = await api.post('/api/v1/student', data, {
+  const getToken = Cookies.get("access_token");
+  const response = await api.post("/api/v1/student", data, {
     headers: {
-      Authorization: `Bearer ${getToken}`
-    }
+      Authorization: `Bearer ${getToken}`,
+    },
   });
   // console.log('response da api login',response.data);
-  return response.data
-}
+  return response.data;
+};
 export const registerUser = async (data) => {
   // console.log('dentro de registerUser ',data.value);
-  const response = await api.post('/api/v1/register', data.value);
+  const response = await api.post("/api/v1/register", data.value);
   // console.log('response da api login',response.data);
-  return response.data
-}
+  return response.data;
+};
+export const getPayments = async (data) => {
+  const getToken = Cookies.get("access_token");
+    console.log('estamos antes da chamada para a api ',data);
+
+  const response = await api.get(`/api/v1/payment/${data}`, {
+    headers: {
+      Authorization: `Bearer ${getToken}`,
+    },
+  });
+
+  return response.data;
+};
